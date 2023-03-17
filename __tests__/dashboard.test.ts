af1308c94894771b6e662015aaaa186ccf6031eb
isai <isaiorellana.dev@gmail.com>
2023-03-17 07:24:56 -0600

    🧪 Prepare test enviroment

---
+++ __tests__/dashboard.test.ts
@@ -0,0 +1,8 @@
+import { describe } from "node:test"
+import TiendasList from "../pages/dashboard/[id]"
+
+describe("Dashboard", () => {
+  it("Renderiza", () => {
+    expect(2 + 2).toBe(4)
+  })
+})
